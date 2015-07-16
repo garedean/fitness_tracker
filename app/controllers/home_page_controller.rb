7779693cad5386189377e81f03c9ba604ce09b88
Peter & Garrett <garrettdolson@gmail.com>
2015-07-16 10:37:08 -0700

    For landing page, use 'home page' scaffolding for RESTful routes

---
+++ app/controllers/home_page_controller.rb
@@ -0,0 +1,27 @@
+class HomePageController < ApplicationController
+  helper_method :sort_column, :sort_direction
+
+  def index
+    @foods = Food.order(sort_column + " " + sort_direction).select(:name, :calories).uniq.paginate(per_page: 10, page: params[:page])
+
+    respond_to do |format|
+
+      format.html {render "index" }
+      format.js
+    end
+  end
+
+  private
+
+  def food_params
+    params.permit(:sort, :direction)
+  end
+
+  def sort_column
+    food_params[:sort] || "name"
+  end
+
+  def sort_direction
+    food_params[:direction] || "asc"
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,5 @@
 Rails.application.routes.draw do
-  root to: "foods#index"
+  root to: "home_page#index"
   devise_for :users
 
   resources :users, only: [:show] do
