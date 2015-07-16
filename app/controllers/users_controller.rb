cfcf1d4f2e77bd00ae4eb70b272719f2a0a4573a
Peter & Garrett <garrettdolson@gmail.com>
2015-07-16 10:18:22 -0700

    Redirect to user show after login. Add calories form to show page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(current_user.id)
+    @new_food = Food.new
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   root to: "foods#index"
   devise_for :users
 
-  resources :users, only: [:show]
-  resources :foods
+  resources :users, only: [:show] do
+    resources :foods
+  end
 end
